8f376068b0c6d21aa3a55cd7fa1545d9839a707d
DeviNoles <devin8821@gmail.com>
2022-11-18 23:30:05 -0500

    Styling updates for list

--- ui-assessment-pokedex-snr/src/components/PokemonList/PokemonList.tsx
+++ ui-assessment-pokedex-snr/src/components/PokemonList/PokemonList.tsx
@@ -27,11 +27,84 @@ import { Search } from '../Search/Search'
         <div key={pkmn.id}>
         <div >{pkmn.number}. {pkmn.name}</div>
    
-        <div>
-          Types: 
+        <div className={classes.typeLayout}>
             {pkmn.types.map((pType) =>
-            <div>{pType}</div>
-          )}
+            {
+            if(pType=="Normal")
+            {
+              
+              return <div className={classes.typeLabel_Normal}>{pType}</div>
+
+            }
+            else if(pType=="Fire"){
+              return <div className={classes.typeLabel_Fire}>{pType}</div>
+
+            }
+            else if(pType=="Water"){
+              return <div className={classes.typeLabel_Water}>{pType}</div>
+
+            }
+            else if(pType=="Electric"){
+              return <div className={classes.typeLabel_Electric}>{pType}</div>
+
+            }
+            else if(pType=="Grass"){
+              return <div className={classes.typeLabel_Grass}>{pType}</div>
+
+            }
+            else if(pType=="Ice"){
+              return <div className={classes.typeLabel_Ice}>{pType}</div>
+
+            }
+            else if(pType=="Fighting"){
+              return <div className={classes.typeLabel_Fighting}>{pType}</div>
+
+            }
+            else if(pType=="Poison"){
+              return <div className={classes.typeLabel_Poison}>{pType}</div>
+
+            }
+            else if(pType=="Ground"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Flying"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Psychic "){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Bug"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Rock"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Ghost"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Dragon"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Dark"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Steel"){
+              return <div>{pType}</div>
+
+            }
+            else if(pType=="Fairy"){
+              return <div>{pType}</div>
+
+            }
+  })}
 
         </div>
         </div>
@@ -56,6 +129,7 @@ const useStyles = createUseStyles(
    
     },
     card:{
+      alignItems: 'center',
       overflow: 'hidden',
       position: 'relative',
       display: 'flex',
@@ -96,17 +170,71 @@ const useStyles = createUseStyles(
   pImage:{
     zIndex: '9'
   },
+  typeLayout:{
+
+    display: 'flex'
+  },
+  typeLabel_Poison:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#A33EA1'
+  },
+  typeLabel_Normal:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#A8A77A'
+  },
+
+  typeLabel_Fighting:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#C22E28'
+  },
+  typeLabel_Ice:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#96D9D6'
+  },
+  typeLabel_Water:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#6390F0'
+  },
+  typeLabel_Fire:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#EE8130'
+  },
+  typeLabel_Electric:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#F7D02C'
+  },
+  typeLabel_Grass:{
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#7AC74C'
+  },
 
   line3: {
     width: '100%',
     height: '6%',
     position: 'absolute',
-    bottom: '35px',
+    bottom: '20%',
     left: '0',
     zIndex: '5',
     backgroundColor: '#0cadf2'
 },
     cardContent:{
+      marginLeft: '2%',
       borderRadius: '17px',
 
       display:'inline'
