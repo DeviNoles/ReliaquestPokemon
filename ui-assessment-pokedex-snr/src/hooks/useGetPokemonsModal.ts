9b6975cbaa7a3435beca8b09fe6bd606f15f3cd5
DeviNoles <devin8821@gmail.com>
2022-11-21 15:17:31 -0500

    Got pokemon data in react route

---
+++ ui-assessment-pokedex-snr/src/hooks/useGetPokemonsModal.ts
@@ -0,0 +1,70 @@
+import { useMemo } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+export type Pokemon = {
+  id: string;
+  name: string;
+  number: string; //<- number = string?? lol
+  image: string;
+  types:string[];
+};
+
+export type PokemonOption = {
+  value: Pokemon['id'];
+  label: Pokemon['name'];
+  number: Pokemon['number'];
+  image: Pokemon['image'];
+  types: Pokemon['types'];
+};
+
+export const GET_POKEMONS_MODAL = gql`
+query pokemon($id: String, $name: String){
+  pokemon(id: $id, name: $name){
+    id
+    number
+    name
+    weight{
+      minimum
+      maximum
+    }
+    height{
+      minimum
+      maximum
+    }
+    classification
+    types
+    resistant
+    weaknesses
+    fleeRate
+    maxCP
+    maxHP
+    image
+  }
+}
+`;
+
+
+
+export const useGetPokemonsModal = () => {
+  const { data, ...queryRes } = useQuery(GET_POKEMONS_MODAL, {
+    variables:{
+    name: "Charizard"
+  }
+  });
+
+  // const pokemons: Pokemon[] = useMemo(() => data?.pokemons || [], [data]);
+
+  // const pokemonOptions: PokemonOption[] = useMemo(
+  //   () => pokemons.map((p: Pokemon) => ({ value: p.id, label: p.name, image: p.image, types: p.types, number: p.number })),
+  //   [pokemons]
+  // );
+  //   console.log(pokemons)
+  //   console.log(pokemonOptions)
+  //   console.log(queryRes)
+  return {
+    data,
+    // pokemonOptions,
+    ...queryRes,
+  };
+};
--- ui-assessment-pokedex-snr/src/screens/Modal.tsx
+++ ui-assessment-pokedex-snr/src/screens/Modal.tsx
@@ -1,68 +1,253 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import { createUseStyles } from 'react-jss';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  useLocation,
-  useParams
-} from "react-router-dom";
-export const Modal = () => {
+import { Link } from 'react-router-dom';
+import { useGetPokemonsModal } from '../hooks/useGetPokemonsModal';
+ export const Modal = () => {
   const classes = useStyles();
-  let history = useNavigate();
-  let { id } = useParams();
-  // let image = IMAGES[parseInt(id, 10)];
+  const { data, loading } = useGetPokemonsModal();
+  const [currentSearch, setCurrentSearch] = useState('');
+  
 
-  // if (!image) return null;
+  if(loading)
+  {
+    return <div className={classes.root}>Loading...</div>
+  }
+  else{
 
-  let back = (e: any) => {
-    e.stopPropagation();
-    history(-1);
-  };
+  console.log(data)
   return (
-    <div
-      onClick={back}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        background: "rgba(0, 0, 0, 0.15)"
-      }}
-    >
-      <div
-        className="root"
-        style={{
-          position: "absolute",
-          background: "#fff",
-          top: 25,
-          left: "10%",
-          right: "10%",
-          padding: 15,
-          border: "2px solid #444"
-        }}
-      >
-        {/* <h1>{image.title}</h1> */}
-        <h1>test123</h1>
-        {/* <Image color={image.color} /> */}
-        <button type="button" onClick={back}>
-          Close
-        </button>
-      </div>
+    <div className={classes.root}>
+
     </div>
   );
 };
-
+ }
 const useStyles = createUseStyles(
   {
     root: {
-      width: '100%',
-      height: '100%',
-      color: 'black'
+      width: '95%',
+      marginLeft: '5%',
+      textAlign: 'center',
+      padding: '32px',
+      boxSizing: 'border-box',
+      color: 'rgba(255,255,255,.92)',
+
     },
+    card:{
+      alignItems: 'center',
+      overflow: 'hidden',
+      position: 'relative',
+      display: 'flex',
+      backgroundColor: '#0d7bdb',
+      marginBottom:'5px',
+      '& img':{
+        height: '100%',
+        width: '150px',
+      },
+      '&:hover':{
+       borderColor: 'orange',
+      },
+      borderColor:'#525252',
+      borderRadius: '17px',
+      borderWidth:'5px',
+      borderStyle:'solid'
+    },
+    
+    line1: {
+        width: '100%',
+        height: '6%',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        zIndex: '5',
+        backgroundColor: '#0cadf2'
+    },
+    line2: {
+      width: '100%',
+      height: '15%',
+      position: 'absolute',
+      bottom: '4px',
+      left: '0',
+      zIndex: '5',
+      backgroundColor: '#0cadf2'
   },
-  { name: 'Modal' }
+
+  pImage:{
+    zIndex: '9'
+  },
+  typeLayout:{
+    display: 'flex',
+    marginTop: '5%'
+  },
+  typeLabel_Poison:{
+
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#A33EA1'
+  },
+  typeLabel_Normal:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#A8A77A'
+  },
+  typeLabel_Fighting:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#C22E28'
+  },
+  typeLabel_Ice:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#96D9D6'
+  },
+  typeLabel_Water:{
+ 
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#6390F0'
+  },
+  typeLabel_Fire:{
+   
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#EE8130'
+  },
+  typeLabel_Electric:{
+
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#F7D02C'
+  },
+  typeLabel_Grass:{
+   
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#7AC74C'
+  },
+  typeLabel_Ground:{
+
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#E2BF65'
+  },
+  typeLabel_Flying:{
+ 
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#A98FF3'
+  },
+  typeLabel_Psychic:{
+   
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#F95587'
+  },
+  typeLabel_Bug:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#A6B91A'
+  },
+  typeLabel_Rock:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#B6A136'
+  },
+  typeLabel_Ghost:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#735797'
+  },
+  typeLabel_Dragon:{
+  
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#6F35FC'
+  },
+  typeLabel_Dark:{
+    
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#705746'
+  },
+  typeLabel_Steel:{
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#B7B7CE'
+  },
+  typeLabel_Fairy:{
+    marginRight: '6%',
+    paddingLeft: '6%',
+    paddingRight: '6%',
+    borderRadius:'17px',
+    backgroundColor: '#D685AD'
+  },
+  line3: {
+    width: '100%',
+    height: '6%',
+    position: 'absolute',
+    bottom: '20%',
+    left: '0',
+    zIndex: '5',
+    backgroundColor: '#0cadf2'
+},
+    cardContent:{
+      marginLeft: '2%',
+      borderRadius: '17px',
+      textDecoration: 'none',
+      display:'inline',
+      color: 'rgba(255,255,255,.92)',
+    },
+    searchBar:{
+      borderRadius:'40px',
+      // border:'2px solid #609',
+      padding: '20px',
+      width: '15%',
+      height: '15px',    
+      color: 'black',
+      marginBottom:'2%'
+
+    }
+  },
+  { name: 'PokemonList' }
 );
